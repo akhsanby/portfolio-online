d411f630487cb10ab8530adec16f2a9f1d98fc1d
AkhsanBy <akhsanby@gmail.com>
2021-09-12 14:43:57 +0700

    remake layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,52 +1,43 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faPhoneAlt, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default class Home extends React.Component {
 	render() {
+		const myIds = [
+			{ icon: faEnvelope, string: "akhsanby@gmail.com" },
+			{ icon: faPhoneAlt, string: "+62 895401224170" },
+			{ icon: faMapMarkerAlt, string: "Central Java, Indonesia"}
+		]
+
 		return(
 			<div id="Home" className="container mx-auto my-auto">
 			  <div className="row">
-			  	<div className="col d-flex justify-content-end">
+			  	<div className="col d-flex justify-content-center gap-5">
 			      <img src="./src/pp.jpg" alt="akhsan" />
-			    </div>
-			    <div className="col d-flex justify-content-start">
 			    	<div>
-				      <h3>
-				      	<span className="badge bg-primary">Hello I'm</span>
-				      </h3>
-				      <h1 className="font-bold">Akhsan Bayu</h1>
-				      <h4>Front-End Developer</h4>
-				      <div>
-				      	<FontAwesomeIcon icon={faEnvelope}/>
-				      	<span className="mx-2">akhsanby@gmail.com</span>
-				      </div>
-				      <div>
-				      	<FontAwesomeIcon icon={faPhoneAlt}/>
-				      	<span className="mx-2">+62 895401224170</span>
-				      </div>
-				      <div>
-				      	<FontAwesomeIcon icon={faMapMarkerAlt}/>
-				      	<span className="mx-2">Central Java, Indonesia</span>
+				      <h3><span className="badge bg-primary">Hello I'm</span></h3>
+				      <h1 className="fw-bolder">Akhsan Bayu</h1>
+				      <h4 className="fw-normal">Front-End Developer</h4>
+				      {myIds.map((myId, index) => 
+					      <div>
+					      	<FontAwesomeIcon icon={myId.icon} color="black" />
+					      	<span className="mx-2">{myId.string}</span>
+					      </div>
+				      )}
+				      <div className="py-4">
+				      	<a href="https://github.com/akhsanby" target="_blank">
+				      		<FontAwesomeIcon icon={faGithub} size="2x" className="mx-2" color="black" />
+				      	</a>
+				      	<a href="https://www.linkedin.com/in/akhsanby/" target="_blank">
+					      	<FontAwesomeIcon icon={faLinkedin} size="2x" className="mx-2" color="black" />
+				      	</a>
 				      </div>
 			    	</div>
 			    </div>
 			  </div>
 			</div>
 		)
-	}
-}
-
-const styles = {
-	badge: {
-		borderTopLeftRadius: "10px",
-		borderTopRightRadius: "10px",
-		borderBottomRightRadius: "10px"
-	},
-	img: {
-		width: "15rem",
-		height: "15rem",
-		borderRadius: "50%"
 	}
 }
